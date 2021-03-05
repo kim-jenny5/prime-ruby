744650080038949db5a565efc584a020f548737a
Jenny Kim <kimj1905@gmail.com>
2021-03-04 20:20:28 -0500

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,34 @@
-# Add  code here!
+def prime?(int)
+    num = 2..(int-1)
+    arr = num.to_a
+    
+    if int < 2
+        return false
+    end
+
+    arr.each do |n|
+        if int % n == 0
+            return false
+        end
+    end
+    return true
+end
+
+
+
+
+
+
+#def prime?(int)
+    #numbers = 1..20
+    #arr = numbers.to_a
+    #arr.each do |n|
+        #if int % n == 0
+            #return false
+        #else
+            #return true
+        ##elsif int % n != 0
+            #return true
+        #end
+    #end
+#end
